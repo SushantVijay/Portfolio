2c8e8baa9ffcd3bb4b59b740aed7df778dcec2de
Sushant Vijay <sushantv.cs2022@citchennai.net>
2023-05-09 21:05:02 +0530

    final

--- script.js
+++ script.js
@@ -11,3 +11,8 @@ const header = document.querySelector("header");
 window.addEventListener ("scroll", function() {
     header.classList.toggle("sticky", this.window.scrollY > 0);
 });
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
